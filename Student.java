6734f43aa53291eb41a6faa333eeaeaa74af233e
Aleksandar-Zoric-93 <dx-apple@hotmail.com>
2015-12-09 11:37:33 +0000

    Added Skeleton of the classes required

---
+++ Student.java
@@ -0,0 +1,30 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author t00166011
+ */
+public class Student {
+    private String forename;
+    private String surname;
+    private Date dob;
+    
+    public void postSchoolMessage(String textMessage)
+    {
+        
+    }
+    
+    public void postClassGroupingMessage(String textMessage)
+    {
+        
+    }
+            
+    
+}
